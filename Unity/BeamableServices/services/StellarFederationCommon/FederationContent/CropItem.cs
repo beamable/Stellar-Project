fd4d8bdfe7a409d94f98e09aff30fce333cb9dc3
moe <mhijazi@beamable.com>
2025-10-08 10:20:03 -0300

    - reverted additions to crop item data

--- Unity/BeamableServices/services/StellarFederationCommon/FederationContent/CropItem.cs
+++ Unity/BeamableServices/services/StellarFederationCommon/FederationContent/CropItem.cs
@@ -3,14 +3,10 @@ using Beamable.Common.Content;
 using Beamable.Common.Inventory;
 using StellarFederationCommon.Extensions;
 using UnityEngine;
+using UnityEngine.AddressableAssets;
 
 namespace StellarFederationCommon.FederationContent
 {
-    
-    /// <summary>
-    /// CropType
-    /// </summary>
-    public enum CropType{ Carrot, Tomato, Pumpkin}
     
     /// <summary>
     /// CropItem NFT
@@ -36,19 +32,8 @@ namespace StellarFederationCommon.FederationContent
 
         [SerializeField] private string name = "";
         [SerializeField] private string description = "";
-        [SerializeField] private string _image = "";
+        [SerializeField] private string image = "";
         [SerializeField] private SerializableDictionaryStringToString customProperties = new SerializableDictionaryStringToString();
-
-        [Header("Crop Details")] 
-        [SerializeField] private int startingSeedAmount;
-        [SerializeField] private int yield;
-        [SerializeField] private int sellingValue;
-        [SerializeField] private CropType cropType;
-        
-        [Header("Crop Sprites")]
-        [SerializeField] private Sprite cropIcon;
-        [SerializeField] private Sprite seedSprite;
-        [SerializeField] private Sprite cropPlantedSprite, cropWateredSprite, cropGrowingSprite, cropRipeSprite;
         
         /// <summary>
         /// Token name
@@ -63,57 +48,13 @@ namespace StellarFederationCommon.FederationContent
         /// <summary>
         /// Token image
         /// </summary>
-        public string Image => _image;
+        public string Image => image;
 
         /// <summary>
         /// Token custom properties
         /// </summary>
         public SerializableDictionaryStringToString CustomProperties => customProperties;
  
-        /// <summary>
-        /// Starting seed amount
-        /// </summary>
-        public int StartingSeedAmount => startingSeedAmount;
-
-        /// <summary>
-        /// Yield
-        /// </summary>
-        public int Yield => yield;
-        
-        /// <summary>
-        /// Selling Value
-        /// </summary>
-        public int SellingValue => sellingValue;
-        
-        /// <summary>
-        /// Crop Type
-        /// </summary>
-        public CropType Type => cropType;
-        
-        /// <summary>
-        /// Crop icon
-        /// </summary>
-        public Sprite CropIcon => cropIcon;
-        /// <summary>
-        /// Seed sprite
-        /// </summary>
-        public Sprite SeedSprite => seedSprite;
-        /// <summary>
-        /// CropPlantedSprite
-        /// </summary>
-        public Sprite CropPlantedSprite => cropPlantedSprite;
-        /// <summary>
-        /// CropWateredSprite
-        /// </summary>
-        public Sprite CropWateredSprite => cropWateredSprite;
-        /// <summary>
-        /// CropGrowingSprite
-        /// </summary>
-        public Sprite CropGrowingSprite => cropGrowingSprite;
-        /// <summary>
-        /// CropRipeSprite
-        /// </summary>
-        public Sprite CropRipeSprite => cropRipeSprite;
     }
 
     /// <summary>
