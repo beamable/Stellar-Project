d9744a98ab38782841e1ffb35206b3a91822c5bc
moe <mhijazi@beamable.com>
2025-10-13 06:15:42 -0300

    - beam coin content fix

--- Unity/BeamableServices/services/StellarFederationCommon/FederationContent/CoinCurrency.cs
+++ Unity/BeamableServices/services/StellarFederationCommon/FederationContent/CoinCurrency.cs
@@ -28,11 +28,11 @@ namespace StellarFederationCommon.FederationContent
             };
         }
 
-        [SerializeField] private readonly string _name = "";
-        [SerializeField] private readonly string _symbol = "";
-        [SerializeField] private readonly int _decimals = 0;
-        [SerializeField] private readonly string _image = "";
-        [SerializeField] private readonly string _description = "";
+        [SerializeField] private string _name = "";
+        [SerializeField] private string _symbol = "";
+        [SerializeField] private int _decimals = 0;
+        [SerializeField] private string _image = "";
+        [SerializeField] private string _description = "";
 
         /// <summary>
         /// name
