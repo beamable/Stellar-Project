fa44d478ad070f9a295c1b2a6355d405cebda1bc
moe <mhijazi@beamable.com>
2025-11-19 09:12:25 -0300

    - giving default crop to player

--- Unity/Assets/Game/Scripts/Beam/BeamInventoryManager.cs
+++ Unity/Assets/Game/Scripts/Beam/BeamInventoryManager.cs
@@ -1,8 +1,11 @@
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using Beamable;
 using Beamable.Common.Api.Inventory;
+using Beamable.Player;
 using Cysharp.Threading.Tasks;
+using Farm.Helpers;
 using StellarFederationCommon.FederationContent;
 using UnityEngine;
 
@@ -15,17 +18,15 @@ namespace Farm.Beam
         private BeamContentManager _contentManager;
         
         public List<PlantInfo> PlayerCrops { get; private set; }
+        public Dictionary<long, PlantInfo> CropInstancesDictionary { get; private set; } = new Dictionary<long, PlantInfo>();
         
         public override async UniTask InitAsync(CancellationToken ct)
         {
             await base.InitAsync(ct);
             _contentManager = BeamManager.Instance.ContentManager;
             PlayerCrops = new List<PlantInfo>();
-            
-            _beamContext.Api.InventoryService.Subscribe(OnInvRefresh);
+            await FetchInventory();
         }
-
-        
 
         public override async UniTask ResetAsync(CancellationToken ct)
         {
@@ -42,15 +43,59 @@ namespace Farm.Beam
             Debug.Log($"Inventory Refreshing...");
             
             //check default crop
-            if (inventoryView.items.Count < 1 || !inventoryView.items.ContainsKey(defaultCropRef.Id))
-            {
-                await _stellarClient.AddItem(defaultCropRef.Id, null);
-            }
-            
-            PlayerCrops.Add(_contentManager.GetCropInfo(defaultCropRef.Id));
-            
-            
+            //if (!await UpdateDefaultCropInfo(inventoryView)) return;
+
             Debug.Log($"Inventory Refresh Done!");
+        }
+
+        public async UniTask FetchInventory()
+        {
+            await _beamContext.Inventory.Refresh();
+            //default crop
+            var inv = await _beamContext.Api.InventoryService.GetCurrent();
+            await UpdateDefaultCropInfo(inv);
+        }
+
+        private async UniTask<bool> UpdateDefaultCropInfo( InventoryView itemsGroup)
+        {
+            var defaultCropInfo = _contentManager.GetCropInfo(defaultCropRef.Id);
+             if (itemsGroup.items.Count < 1 || !itemsGroup.items.ContainsKey(defaultCropRef.Id))
+             {
+                 var properties = new Dictionary<string, string>
+                 {
+                     { GameConstants.SeedsLeftProp, defaultCropInfo.seedsToPlant.ToString() },
+                     { GameConstants.YieldProp, defaultCropInfo.cropData.yield.ToString() }
+                 };
+                 await _stellarClient.AddItem(defaultCropRef.Id, properties);
+                 return false;
+             }
+
+            var cropInstances = itemsGroup.items[defaultCropRef.Id];
+             var cropInstance = cropInstances[0];
+            
+             defaultCropInfo.instanceId = cropInstance.id;
+             defaultCropInfo.yieldAmount = cropInstance.properties.TryGetValue(GameConstants.YieldProp, out var yieldAmount) ? int.Parse(yieldAmount) : 0;
+             defaultCropInfo.seedsToPlant = cropInstance.properties.TryGetValue(GameConstants.SeedsLeftProp, out var seedsAmount) ? int.Parse(seedsAmount) : 0;
+            
+             var defaultPlayerCrop = TryGetCropInfo(defaultCropInfo.instanceId);
+             if (defaultPlayerCrop == null)
+             {
+                 CropInstancesDictionary.Add(defaultCropInfo.instanceId, defaultCropInfo);
+                 PlayerCrops.Add(defaultCropInfo);
+             }
+             else
+             {
+                 CropInstancesDictionary[cropInstance.id] = defaultCropInfo;
+                 var index = PlayerCrops.FindIndex(x => x.instanceId == cropInstance.id);
+                 PlayerCrops[index] = defaultCropInfo;
+             }
+
+            return true;
+        }
+
+        public PlantInfo TryGetCropInfo(long instanceId)
+        {
+            return CropInstancesDictionary.GetValueOrDefault(instanceId);
         }
     }
 }
