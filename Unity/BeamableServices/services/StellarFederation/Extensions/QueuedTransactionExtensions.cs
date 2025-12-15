b0d2de58cca299d2dd6db1b4bbd7dda3c9ecc9d5
zpatekar <77490300+zpatekar@users.noreply.github.com>
2025-12-15 17:16:58 +0100

    Add Commerce listings.

--- Unity/BeamableServices/services/StellarFederation/Extensions/QueuedTransactionExtensions.cs
+++ Unity/BeamableServices/services/StellarFederation/Extensions/QueuedTransactionExtensions.cs
@@ -1,35 +1,35 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Beamable.Common.Content;
-using Beamable.StellarFederation.Features.Transactions.Models;
-using Beamable.StellarFederation.Features.Transactions.Storage.Models;
-
-namespace Beamable.StellarFederation.Extensions;
-
-public static class QueuedTransactionExtensions
-{
-    public static async Task<List<QueuedTransactionModel>> TransformWithKey(this List<QueuedTransactionBase> transactions,
-        Func<string, Task<IContentObject>> fetchValueAsync)
-    {
-        var tasks = transactions.Select(async tx => new QueuedTransactionModel
-        (
-            tx,
-            await fetchValueAsync(tx.ConcurrencyKey)
-        ));
-        return (await Task.WhenAll(tasks)).ToList();
-    }
-
-    public static async Task<List<QueuedTransactionModel>> TransformWithKeys(this List<QueuedTransactionBase> transactions,
-        Func<IEnumerable<string>, Task<List<IContentObject>>> fetchValuesAsync)
-    {
-        var keys = transactions.Select(t => t.ConcurrencyKey);
-        var valueMap = await fetchValuesAsync(keys);
-        return transactions.Select(tx => new QueuedTransactionModel
-        (
-            tx,
-            valueMap.First(v => v.Id == tx.ConcurrencyKey)
-        )).ToList();
-    }
-}
+// using System;
+// using System.Collections.Generic;
+// using System.Linq;
+// using System.Threading.Tasks;
+// using Beamable.Common.Content;
+// using Beamable.StellarFederation.Features.Transactions.Models;
+// using Beamable.StellarFederation.Features.Transactions.Storage.Models;
+//
+// namespace Beamable.StellarFederation.Extensions;
+//
+// public static class QueuedTransactionExtensions
+// {
+//     public static async Task<List<QueuedTransactionModel>> TransformWithKey(this List<QueuedTransactionBase> transactions,
+//         Func<string, Task<IContentObject>> fetchValueAsync)
+//     {
+//         var tasks = transactions.Select(async tx => new QueuedTransactionModel
+//         (
+//             tx,
+//             await fetchValueAsync(tx.ConcurrencyKey)
+//         ));
+//         return (await Task.WhenAll(tasks)).ToList();
+//     }
+//
+//     public static async Task<List<QueuedTransactionModel>> TransformWithKeys(this List<QueuedTransactionBase> transactions,
+//         Func<IEnumerable<string>, Task<List<IContentObject>>> fetchValuesAsync)
+//     {
+//         var keys = transactions.Select(t => t.ConcurrencyKey);
+//         var valueMap = await fetchValuesAsync(keys);
+//         return transactions.Select(tx => new QueuedTransactionModel
+//         (
+//             tx,
+//             valueMap.First(v => v.Id == tx.ConcurrencyKey)
+//         )).ToList();
+//     }
+// }
--- Unity/BeamableServices/services/StellarFederation/Features/BlockProcessor/Decoder/MintEventDto.cs
+++ Unity/BeamableServices/services/StellarFederation/Features/BlockProcessor/Decoder/MintEventDto.cs
@@ -2,6 +2,6 @@ namespace Beamable.StellarFederation.Features.BlockProcessor.Decoder;
 
 public class MintEventDto
 {
-    public string Wallet { get; set; }
+    public string Wallet { get; set; } = "";
     public long Amount { get; set; }
 }
